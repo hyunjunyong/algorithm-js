54f751edb4cb8d4e79c742d21cb35121bf5f19fb
현준용 <hjy4649@cocen.com>
2022-08-10 13:32:11 +0900

    removeDuplicates 이거 /***/에러 sucess

--- leetcode/removeDuplicates.js
+++ leetcode/removeDuplicates.js
@@ -3,17 +3,12 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-  console.log(nums);
   for (i = 0; i < nums.length; i++) {
     if (nums[i] === nums[i + 1]) {
       nums.splice(i, 1);
       i--;
     }
   }
-  console.log(nums);
-  return nums;
 };
 
-removeDuplicates([1, 1, 2]);
-
-//아니 왜안되;;;
+removeDuplicates([1, 1, 1, 2, 3]);
