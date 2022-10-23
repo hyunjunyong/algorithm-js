5d7d93d9397e097c834fb76f74e9c7ba06a42705
현준용 <hjy4649@cocen.com>
2022-10-23 16:04:58 +0900

    4solve easy

--- 프로그래머스0단계/1.js
+++ 프로그래머스0단계/1.js
@@ -11,3 +11,23 @@ function solution(num1, num2) {
 function solution(num1, num2) {
   return num1 * num2;
 }
+
+//나누기
+function solution(num1, num2) {
+  return num1 % num2;
+}
+
+//더하기
+function solution(num1, num2) {
+  return num1 + num2;
+}
+
+//나이구하기
+function solution(age) {
+  return 2022 - age + 1;
+}
+
+//두수 비교하기
+function solution(num1, num2) {
+  return num1 === num2 ? 1 : -1;
+}
