bb2b8a23596a16bba997101b6e8d37198d0bffa0
현준용 <hjy4649@cocen.com>
2022-10-23 16:23:25 +0900

    4solve easy

--- 프로그래머스0단계/1.js
+++ 프로그래머스0단계/1.js
@@ -31,3 +31,34 @@ function solution(age) {
 function solution(num1, num2) {
   return num1 === num2 ? 1 : -1;
 }
+
+//각도 계산
+function solution(angle) {
+  if (angle < 90) return 1;
+  else if (angle === 90) return 2;
+  else if (angle < 180) return 3;
+  else if (angle === 180) return 4;
+}
+
+// 두수의 나눗셈
+function solution(num1, num2) {
+  return parseInt((num1 / num2) * 1000);
+}
+
+//배열의 평균
+function solution(numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum / numbers.length;
+}
+
+//중복된 숫자 구하기
+function solution(array, n) {
+  var answer = 0;
+  array.map((item) => {
+    if (item === n) answer++;
+  });
+  return answer;
+}
