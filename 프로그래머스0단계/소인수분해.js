efe1562473f07a52c0e7f67348042fc9287b121a
현준용 <hjy4649@cocen.com>
2023-02-12 15:03:03 +0900

    이 개멍청아

--- 프로그래머스0단계/소인수분해.js
+++ 프로그래머스0단계/소인수분해.js
@@ -4,7 +4,7 @@ function solution(n) {
   while (n !== 1) {
     if (n % i === 0) {
       answer.push(i);
-      n = n / 2;
+      n = n / i;
       i = 2;
     } else {
       i++;
@@ -13,21 +13,4 @@ function solution(n) {
   return [...new Set(answer)];
 }
 
-function solution(n) {
-  const smallNumber = [];
-  let idx = 2;
-  // n이 0이 될때까지
-  while (n !== 1) {
-    // n의 약수라면 배열에 추가
-    if (n % idx === 0) {
-      smallNumber.push(idx);
-      n /= idx;
-      idx = 2;
-    } else {
-      idx += 1;
-    }
-  }
-  // 중복 제거하여 반환
-  return Array.from(new Set(smallNumber));
-}
 console.log(solution(12));
