1327540fcc0b45f67ecabe6092a6d08b8c09d551
현준용 <hjy4649@cocen.com>
2023-02-15 16:51:57 +0900

    왜 이게 답인지 모르겠음

--- dongbinna/큰수의법칙.js
+++ dongbinna/큰수의법칙.js
@@ -2,4 +2,21 @@
 // 5 8 3
 // 2 4 5 4 6
 // output 46
-function resolve(n, k, m) {}
+function resolve(n, m, k, array) {
+  let sortArray = array.slice().sort((a, b) => a - b);
+  let answer = 0;
+  while (m > 0) {
+    let i = n - 1;
+    if (i === n - 1) {
+      answer += sortArray[i] * k;
+      i = n - 2;
+      m -= k;
+    } else if (i === n - 2) {
+      answer += sortArray[i];
+      i = n - 1;
+      m -= 1;
+    }
+  }
+  return answer;
+}
+console.log(resolve(5, 8, 3, [2, 4, 5, 4, 6]));
