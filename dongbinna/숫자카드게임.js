66d207f792ae1980b35b2904cf9d0845fe77731a
현준용 <hjy4649@cocen.com>
2023-02-16 10:33:36 +0900

    동빈나 숫자카드게임

---
+++ dongbinna/숫자카드게임.js
@@ -0,0 +1,23 @@
+function resolve(n, m, array) {
+  let leastNumber = [];
+  for (let i = 0; i < n; i++) {
+    array[i].sort((a, b) => a - b);
+    leastNumber.push(array[i][0]);
+  }
+  leastNumber.sort((a, b) => a - b);
+  console.log(leastNumber);
+  return leastNumber[leastNumber.length - 1];
+}
+console.log(
+  resolve(3, 3, [
+    [3, 1, 2],
+    [4, 1, 4],
+    [2, 2, 2],
+  ])
+);
+console.log(
+  resolve(2, 4, [
+    [7, 3, 1, 8],
+    [3, 3, 3, 4],
+  ])
+);
