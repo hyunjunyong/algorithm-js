426c9a9574eaba0c61c6762190b3a14e45f7bcad
현준용 <hjy4649@cocen.com>
2023-02-18 16:10:22 +0900

    문제 못풀겠다

--- 프로그래머스0단계/hideNumber.js
+++ 프로그래머스0단계/hideNumber.js
@@ -4,17 +4,21 @@ function solution(my_string) {
   let hideNumber = [];
   for (let i = 0; i < myArray.length; i++) {
     if (!isNaN(myArray[i])) {
-      if (!isNaN(myArray[i - 1])) {
-        hideNumber[hideNumber.length - 1] =
-          hideNumber[hideNumber.length - 1] * 10 + Number(myArray[i]);
+      if (!isNaN(myArray[i + 1])) {
       } else {
         hideNumber.push(Number(myArray[i]));
       }
     }
+    console.log(hideNumber);
   }
-  console.log(hideNumber);
-  answer = hideNumber.reduce((a, b) => a + b);
+
   return answer;
 }
 
 console.log(solution('aAb1B2cC34oOp'));
+
+// 다른사람 의견
+// function solution(my_string) {
+//   const nums = my_string.match(/[0-9]+/g);
+//   return nums ? nums.map(num => +num).reduce((a, c) => a + c, 0) : 0;
+// }
