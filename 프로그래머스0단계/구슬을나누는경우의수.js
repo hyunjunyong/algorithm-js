d84a8432e171b5c1e8c14bb721c6cb42df4a9c14
현준용 <hjy4649@cocen.com>
2023-02-25 17:11:43 +0900

    test case error

---
+++ 프로그래머스0단계/구슬을나누는경우의수.js
@@ -0,0 +1,18 @@
+function solution(balls, share) {
+  var answer = 0;
+  answer = factorial(balls) / (factorial(balls - share) * factorial(share));
+  return answer;
+}
+
+function factorial(n) {
+  let result = 1;
+  if (n === 1) return 1;
+  else {
+    for (let i = 2; i <= n; i++) {
+      result *= i;
+    }
+    return result;
+  }
+}
+
+console.log(solution());
