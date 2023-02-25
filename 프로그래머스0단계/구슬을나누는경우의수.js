a007a4f96f923472392f6b0b9280df26f1b35748
현준용 <hjy4649@cocen.com>
2023-02-25 17:21:20 +0900

    왜 math.round를 사용해야하는

--- 프로그래머스0단계/구슬을나누는경우의수.js
+++ 프로그래머스0단계/구슬을나누는경우의수.js
@@ -16,3 +16,5 @@ function factorial(n) {
 }
 
 console.log(solution(5, 4));
+// https://joooing.tistory.com/entry/Javascript-%EC%86%8C%EC%88%98%EC%A0%90floating-point-%EA%B3%84%EC%82%B0-%EC%98%A4%EB%A5%98
+// 왜 Math.round를 써야하는가
