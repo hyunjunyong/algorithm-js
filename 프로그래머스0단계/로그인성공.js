99eb9c8dda86b43e0e8c388aff2ad7ba97bd441a
현준용 <hjy4649@cocen.com>
2023-02-27 11:24:54 +0900

    로그인 다른 풀이

--- 프로그래머스0단계/로그인성공.js
+++ 프로그래머스0단계/로그인성공.js
@@ -22,3 +22,9 @@ console.log(
     ]
   )
 );
+
+// function solution(id_pw, db) {
+//   const [id, pw] = id_pw;
+//   const map = new Map(db);
+//   return map.has(id) ? (map.get(id) === pw ? 'login' : 'wrong pw') : 'fail';
+// }
