99066d711f40f80521a98c8f32c8d010ce592070
현준용 <hjy4649@cocen.com>
2023-03-07 14:01:56 +0900

    ready

---
+++ 프로그래머스1단계/가장가까운같은글자.js
@@ -0,0 +1,9 @@
+function solution(s) {
+  var answer = [];
+  return answer;
+}
+
+console.log(solution('banana'));
+// [-1, -1, -1, 2, 2, 2]
+console.log(solution('foobar'));
+// [-1, -1, 1, -1, -1, -1]
