24579d1264b48701494e3fd0f918f8eef3379f92
현준용 <hjy4649@cocen.com>
2023-03-08 11:37:00 +0900

    가장가까운같은글자

--- 프로그래머스1단계/가장가까운같은글자.js
+++ 프로그래머스1단계/가장가까운같은글자.js
@@ -1,5 +1,16 @@
 function solution(s) {
   var answer = [];
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (stack.includes(s[i])) {
+      answer.push(stack.length - stack.lastIndexOf(s[i]));
+      stack.push(s[i]);
+    } else {
+      answer.push(-1);
+      stack.push(s[i]);
+    }
+  }
+
   return answer;
 }
 
